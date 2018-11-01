d723990b7731302ed118f04e84fc058f5853972d
Matt <bedrocks@gmail.com>
2018-10-31 23:27:18 -0500

    Finish cleaning up requests into a dataframe.

--- bin/gfk-publicstuff.R
+++ bin/gfk-publicstuff.R
@@ -5,6 +5,7 @@
 
 library(jsonlite)
 library(rjson)
+library(dplyr)
 
 # Grab city view for Grand Forks
 gfk <- rjson::fromJSON(file="https://www.publicstuff.com/api/2.1/city_view?space_id=15174")
@@ -17,17 +18,35 @@ names(gfk_request_types) <- c("request_type_id","request_type_name")
 # Unix timestamp from a week ago
 today <- as.numeric(as.POSIXct(Sys.time()))
 week_ago <- today-604800
-
+# For all request types, get (at most) 10 requests from the last week from the PublicStuff API.
 gfk_requests <- lapply(gfk_request_types$request_type_id,
                        function(x) jsonlite::fromJSON(paste0("https://www.publicstuff.com/api/2.1/requests_list?request_type_id=",
                                                         x,"&after_timestamp=",week_ago,"&limit=10")))
-#gfk_requests1[sapply(gfk_requests1 , is.null)] <- NULL
-
+# Pull out exactly the data we need
 gfk_requests <- lapply(gfk_requests, function(x) x$response$requests$request)
+# Drop null list items
+gfk_requests <- Filter(Negate(is.null), gfk_requests)
+# Drop images (in fact, there is image_thumbnail in the data we want,
+# and we can just replace small_ with large_ to get a bigger image later!)
+drop_image <- function(x){
+    if(class(x$primary_attachment) == "data.frame") {
+        x$primary_attachment <- NULL
+        }
+    return(x)
+}
+gfk_requests <- lapply(gfk_requests, drop_image)
+# Put the requests together in a data frame
+gfk_requests <- bind_rows(gfk_requests)
 
 
-## Need to clear out attachments in some way here
+#### Tweeting
 
-# Drop nulls
-b<-Filter(Negate(is.null), gfk_requests)
-gfk_requests <- dplyr::bind_rows(b)
+# After tweeting, write a small text file that has the last timestamp that was tweeted. Use that for grabbing future requests.
+
+
+
+
+
+
+
+
